95163b1b09947c5371a701b37cbfac07f863a4f9
Rahul Rajput <rockrahul8148@gmail.com>
2026-03-30 09:13:50 +0530

    Update auto_block.sh

--- auto_block.sh
+++ auto_block.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-grep "FAIL LOGIN" /var/log/vsftpd.log | awk '{print $NF}' | sort | uniq -c | while read count ip
+grep "FAIL LOGIN" /var/log/vsftpd.log | awk '{print $(NF-3)}' | sort | uniq -c | while read count ip
 do
-    if [ "$count" -gt 5 ]; then
+    if [ $count -gt 5 ]; then
         echo "Blocking $ip"
-        iptables -A INPUT -s $ip -j DROP
+
+        iptables -C INPUT -s $ip -j DROP 2>/dev/null
+        if [ $? -ne 0 ]; then
+            iptables -A INPUT -s $ip -j DROP
+        fi
     fi
 done
